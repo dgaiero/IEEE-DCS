abcd0a44ed8c49b6c31bcc9160bcd797266bd3e1
RussellCaletena <RussellCaletena@gmail.com>
2017-12-14 23:01:50 -0800

    Add first view for each app

--- SPL/SPL/urls.py
+++ SPL/SPL/urls.py
@@ -13,9 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+####################################################
+#This is the master urls.py file for the SPL Project
+####################################################
+
+from django.urls import include, path
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
+    path('app_Another_Action/', include('app_Another_Action.urls')),
+    path('app_checkIn/', include('app_CheckIn.urls')),
+    path('app_CheckIn_Or_CheckOut/', include('app_CheckIn_Or_CheckOut.urls')),
+    path('app_CheckOut/', include('app_CheckOut.urls')),
+    path('app_CheckOut_Express/', include('app_CheckOut_Express.urls')),
+    path('app_CheckOut_Reg/', include('app_CheckOut_Reg.urls')),
+    path('app_Registration/', include('app_Registration.urls')),
+    path('app_Swipe_PolyCard/', include('app_Swipe_PolyCard.urls')),
+    path('app_Transaction_Summary/', include('app_Transaction_Summary.urls')),
     path('admin/', admin.site.urls),
 ]
--- SPL/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-from django.contrib import admin
-
-urlpatterns = [
-    path('app_Swipe_PolyCard/', include('app_Swipe_PolyCard.urls')),
-    path('admin/', admin.site.urls),
-]
