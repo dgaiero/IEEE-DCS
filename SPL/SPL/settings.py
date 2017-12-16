8821743504f7422345a414f28cf7ea91b22bbf78
RussellCaletena <RussellCaletena@gmail.com>
2017-12-16 13:41:42 -0800

    Create models for app_Transaction and initiate migrations

--- SPL/SPL/settings.py
+++ SPL/SPL/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'app_Registration.apps.AppTransactionConfig',
+    'app_Transaction.apps.AppTransactionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- SPL/app_Transaction/models.py
+++ SPL/app_Transaction/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class Part(models.Model):
+    part_Name_Text              = models.CharField(max_length=100)
+    quantity_Number             = models.IntegerField(default=0)
+    quantity_Checked_Out_Number = models.IntegerField(default=0)
+    #ID                  = models.IntegerField(blank=True, null=True)
+
+class User(models.Model):
+    first_Name_text     = models.CharField(max_length=100)
+    last_Name_text      = models.CharField(max_length=100)
+    user_Type_text      = models.CharField(max_length=100)
+    iso_Number          = models.IntegerField(default=0)
+    library_Code_Number = models.IntegerField(default=0)
+    parts               = models.ManyToManyField(Part)
+
+class Transaction(models.Model):
+    #userid
+    #part
+    #date
+    pass
+
+class Lab(models.Model):
+    course_Name_Text = models.CharField(max_length=100)
+    section_Number   = models.IntegerField(default=0)
+    week_Number      = models.IntegerField(default=0)
+
+class Log(models.Model):
+    #date_And_Time        = models.DateTimeField(default=datetime.now(), blank=True)
+    client_Name_text     = models.CharField(max_length=100)
+    tech_Name_text       = models.CharField(max_length=100)
+    #part checked out
+    quantity_Number      = models.IntegerField(default=0)
+    transaction_Complete = models.BooleanField(default=False)
