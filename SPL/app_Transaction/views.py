df72c8cfd8a5691cf7b747a4fbc71bd5a0e10169
RussellCaletena <RussellCaletena@gmail.com>
2017-12-16 23:41:05 -0800

    Relocate templates subdirectory and create index.html

--- SPL/app_Transaction/views.py
+++ SPL/app_Transaction/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
+    template = loader.get_template('app_Transaction/index.html')
+    #return HttpResponse(template.render(request))
     return HttpResponse("Hello, world. You're at the app_Transaction index.")
