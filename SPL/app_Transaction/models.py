74f0634b686313bde6fab23a7eca2438c95460e1
dgaiero <dpgaiero@gmail.com>
2017-12-17 22:24:57 -0800

    Fixed model

--- SPL/app_Transaction/models.py
+++ SPL/app_Transaction/models.py
@@ -11,6 +11,7 @@ class User(models.Model):
     first_Name_text     = models.CharField(max_length=100)
     last_Name_text      = models.CharField(max_length=100)
     user_Type_text      = models.CharField(max_length=100)
+    user_email          = models.CharField(max_length=100)
     iso_Number          = models.IntegerField(default=0)
     library_Code_Number = models.IntegerField(default=0)
     parts               = models.ManyToManyField(Part)
@@ -28,7 +29,7 @@ class Lab(models.Model):
     parts            = models.ManyToManyField(Part)
 
 class Log(models.Model):
-    #date_And_Time        = models.DateTimeField(default=datetime.now(), blank=True)
+    date_And_Time        = models.DateTimeField(default=datetime.datetime.now(), blank=True)
     client_Name_text     = models.CharField(max_length=100)
     tech_Name_text       = models.CharField(max_length=100)
     #part checked out
