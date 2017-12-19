417f242c92dd7997901e0dd1bef5d4ee8bdc87a9
RussellCaletena <RussellCaletena@gmail.com>
2017-12-18 16:59:32 -0800

    Add EELabsParts, parts, and some comments

--- SPL/app_Transaction/models.py
+++ SPL/app_Transaction/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 import datetime
 
+# Create Part model with necessary attributes
 class Part(models.Model):
     part_Name_Text              = models.CharField(max_length=100)
     quantity_Number             = models.IntegerField(default=0)
     quantity_Checked_Out_Number = models.IntegerField(default=0)
     id_Number                   = models.IntegerField(blank=True, null=True)
 
+# Create User model with necessary attributes
 class User(models.Model):
     first_Name_text     = models.CharField(max_length=100)
     last_Name_text      = models.CharField(max_length=100)
@@ -16,22 +18,27 @@ class User(models.Model):
     library_Code_Number = models.IntegerField(default=0)
     parts               = models.ManyToManyField(Part)
 
+# Create Transaction model with necessary attributes
+# We'll worry about this later...
+# What is the difference between transaction and log?
 class Transaction(models.Model):
     #userid
     #part
     #date
     pass
 
+# Create Lab model with necessary attributes
 class Lab(models.Model):
     course_Name_Text = models.CharField(max_length=100)
     section_Number   = models.IntegerField(default=0)
     week_Number      = models.IntegerField(default=0)
     parts            = models.ManyToManyField(Part)
+    # possible things to add: professor name
 
+# Create Log model with necessary attributes
 class Log(models.Model):
     date_And_Time        = models.DateTimeField(default=datetime.datetime.now(), blank=True)
     client_Name_text     = models.CharField(max_length=100)
     tech_Name_text       = models.CharField(max_length=100)
-    #part checked out
-    quantity_Number      = models.IntegerField(default=0)
+    parts                = models.ManyToManyField(Part)
     transaction_Complete = models.BooleanField(default=False)
