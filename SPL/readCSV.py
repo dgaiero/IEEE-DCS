e061cb18df960a0ed558b824280739b45feed403
RussellCaletena <RussellCaletena@gmail.com>
2017-12-19 17:53:39 -0800

    Add comments

--- SPL/readCSV.py
+++ SPL/readCSV.py
@@ -1,15 +1,17 @@
 # Reading parts CSV file, making them Django objects, and storing them in a database
 
-import csv
-
+# Bypassing manage.py
+# Set DJANGO_SETTINGS_MODULE enviroment variable to SPL.settings
 import os
 os.environ['DJANGO_SETTINGS_MODULE']='SPL.settings'
 
 import django
 django.setup()
 
+import csv
 from app_Transaction.models import Part
 
+# Take parts in CSV file, create Django parts objects, and save them in database
 with open('splParts.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
