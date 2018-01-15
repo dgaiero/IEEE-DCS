62764ef833b612117da3e5853c0283930041b82d
dgaiero <dpgaiero@gmail.com>
2018-01-15 10:32:36 -0800

    Changes to settings.py

--- SPL/SPL/settings.py
+++ SPL/SPL/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '***REMOVED***'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.137.1',]
+ALLOWED_HOSTS = []
 
 
 # Application definition
