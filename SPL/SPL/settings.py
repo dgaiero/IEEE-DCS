63b66aa01b6ce3a29a1ee839611eb63e37f3356a
dgaiero <dpgaiero@gmail.com>
2018-04-08 17:19:32 -0700

    update static dir

--- SPL/SPL/settings.py
+++ SPL/SPL/settings.py
@@ -144,8 +144,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-# STATIC_URL = 'http://storage.googleapis.com/ieee-members-static/'
-STATIC_URL = '/static/'
+STATIC_URL = 'http://storage.googleapis.com/ieee-members-static/staticfiles/'
+# STATIC_URL = '/static/'
 
 # Define a list of directories for static files
 STATICFILES_DIRS = [
