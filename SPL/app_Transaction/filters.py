64865e9b6e790acabdf6046becc850ee8ca2e93e
dgaiero <dpgaiero@gmail.com>
2018-04-11 21:30:34 -0700

    fixed search user expiration year filter

--- SPL/app_Transaction/filters.py
+++ SPL/app_Transaction/filters.py
@@ -1,14 +1,25 @@
-from .models import User
 import django_filters
+
+from .models import User
 
 
 class UserFilter(django_filters.FilterSet):
-    first_Name = django_filters.CharFilter(lookup_expr='icontains')
-    last_Name = django_filters.CharFilter(lookup_expr='icontains')
+    # first_Name = django_filters.CharFilter(lookup_expr='icontains')
+    # last_Name = django_filters.CharFilter(lookup_expr='icontains')
     cal_Poly_Email = django_filters.CharFilter(lookup_expr='icontains')
-    year_expired = django_filters.NumberFilter(name='ieee_member_expiration_date', lookup_expr='year')
+    ieee_member_expiration_date = django_filters.NumberFilter(name='ieee_member_expiration_date', lookup_expr='year')
 
     class Meta:
         model = User
-        fields = ['first_Name', 'last_Name', 'userType', 'cal_Poly_Email', 'ieee_member_number',
-                  'ieee_member_expiration_date', 'phone_Number', 'polyCard_Data']
+        fields = {
+            'first_Name': ['exact', 'icontains'],
+            'last_Name': ['exact', 'icontains'],
+            'userType': ['exact'],
+            'cal_Poly_Email': ['icontains'],
+            'ieee_member_number': ['exact', 'icontains'],
+            'ieee_member_expiration_date': ['year'],
+            'phone_Number': ['exact', 'icontains'],
+            'polyCard_Data': ['exact'],
+        }
+        # fields = ['first_Name', 'last_Name', 'userType', 'cal_Poly_Email', 'ieee_member_number',
+        #           'ieee_member_expiration_date', 'phone_Number', 'polyCard_Data']
